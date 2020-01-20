8edbf7128732f000c69094fffeae92abf1af0cce
philipalexanderols <philipalexanderols@users.noreply.github.com>
2020-01-20 15:53:15 +0100

    la till javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("hejsan");
+
+document.getElementById("bild60").onclick = () => {
+    document.getElementById("bild60").classList.toggle("mindrebild");
+}
+document.getElementById("bild70").onclick = () => {
+    document.getElementById("bild70").classList.toggle("mindrebild");
+}
+document.getElementById("bild80").onclick = () => {
+    document.getElementById("bild80").classList.toggle("mindrebild");
+}
+document.getElementById("bild90").onclick = () => {
+    document.getElementById("bild90").classList.toggle("mindrebild");
+}
+document.getElementById("bild00").onclick = () => {
+    document.getElementById("bild00").classList.toggle("mindrebild");
+}
+document.getElementById("bild10").onclick = () => {
+    document.getElementById("bild10").classList.toggle("mindrebild");
+}
